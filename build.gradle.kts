79302ec3115b09df18c63e464316aa5cbb8b0a21
CPJiNan <2759278070@qq.com>
2023-07-30 21:47:26 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("io.izzel.taboolib") version "1.56"
+    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+}
+
+taboolib {
+    install("common")
+    install("common-5")
+    install("module-chat")
+    install("module-configuration")
+    install("module-kether")
+    install("module-lang")
+    install("module-metrics")
+    install("platform-bukkit")
+    install("expansion-command-helper")
+    classifier = null
+    version = "6.0.11-31"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("ink.ptms:nms-all:1.0.0")
+    compileOnly("ink.ptms.core:v11902:11902-minimize:mapped")
+    compileOnly("ink.ptms.core:v11902:11902-minimize:universal")
+    compileOnly(kotlin("stdlib"))
+    compileOnly(fileTree("libs"))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjvm-default=all")
+    }
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://repo.tabooproject.org/repository/releases")
+            credentials {
+                username = project.findProperty("taboolibUsername").toString()
+                password = project.findProperty("taboolibPassword").toString()
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("library") {
+            from(components["java"])
+            groupId = project.group.toString()
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/cpjinan/PlayerLevel.kt
@@ -0,0 +1,11 @@
+package com.github.cpjinan
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object PlayerLevel : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
