c509aea48e2ce280f2551978d8986f25abae1ae2
CPJiNan <2759278070@qq.com>
2024-02-11 23:11:51 +0800

    [3.0.5] [!]修复了属性拓展相关内容

--- src/main/kotlin/com/github/cpjinan/plugin/akarilevel/internal/hook/Attribute.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akarilevel/internal/hook/Attribute.kt
@@ -2,33 +2,41 @@ package com.github.cpjinan.plugin.akarilevel.internal.hook
 
 import ac.github.oa.api.OriginAttributeAPI
 import ac.github.oa.internal.core.attribute.impl.ExpAddon
+import com.github.cpjinan.plugin.akarilevel.api.event.exp.PlayerExpChangeEvent
 import com.github.cpjinan.plugin.akarilevel.internal.manager.ConfigManager
 import github.saukiya.sxattribute.SXAttribute
-import org.bukkit.entity.Player
+import org.bukkit.Bukkit
 import org.serverct.ersha.AttributePlus
+import taboolib.common.platform.event.SubscribeEvent
 import taboolib.common5.compileJS
+import kotlin.math.roundToInt
 
 object Attribute {
-    fun getAddition(player: Player, exp: Int, source: String): Int {
-        if (ConfigManager.isEnabledAttribute() && source in ConfigManager.getAttributeSource()) {
+    @SubscribeEvent
+    fun onPlayerExpChange(event: PlayerExpChangeEvent) {
+        var exp = event.exp
+        if (ConfigManager.isEnabledAttribute() && event.source in ConfigManager.getAttributeSource()) {
             var attributeValue: Number = 0
             when (ConfigManager.getAttributePlugin()) {
-                "AttributePlus" -> attributeValue =
-                    AttributePlus.attributeManager.getAttributeData(player.uniqueId, player)
+                "AttributePlus" -> if (Bukkit.getServer().pluginManager.isPluginEnabled("AttributePlus")) attributeValue =
+                    AttributePlus.attributeManager.getAttributeData(event.player)
                         .getAttributeValue(ConfigManager.getAttributeName())[0]
 
-                "SX-Attribute" -> attributeValue =
-                    SXAttribute.getApi().getEntityData(player).getValues(ConfigManager.getAttributeName())[0]
+                "SX-Attribute" -> if (Bukkit.getServer().pluginManager.isPluginEnabled("SX-Attribute")) attributeValue =
+                    SXAttribute.getApi().getEntityData(event.player).getValues(ConfigManager.getAttributeName())[0]
 
-                "OriginAttribute" -> attributeValue =
-                    OriginAttributeAPI.getAttributeData(player).getData(ExpAddon().index, ExpAddon.DefaultImpl().index)
+                "OriginAttribute" -> if (Bukkit.getServer().pluginManager.isPluginEnabled("OriginAttribute")) attributeValue =
+                    OriginAttributeAPI.getAttributeData(event.player)
+                        .getData(ExpAddon().index, ExpAddon.DefaultImpl().index)
                         .get(ExpAddon.DefaultImpl().index)
+
+                else -> throw IllegalArgumentException("Unsupported attribute plugin ${ConfigManager.getAttributePlugin()}.")
             }
-            return ConfigManager.getAttributeFormula()
-                .replace("%exp%", exp.toString(), true)
+            exp = ConfigManager.getAttributeFormula()
+                .replace("%exp%", event.exp.toString(), true)
                 .replace("%attribute%", attributeValue.toDouble().toString(), true)
-                .compileJS()?.eval()?.toString()?.toIntOrNull() ?: return exp
+                .compileJS()?.eval()?.toString()?.toDouble()?.roundToInt() ?: event.exp
         }
-        return exp
+        event.exp = exp
     }
 }
