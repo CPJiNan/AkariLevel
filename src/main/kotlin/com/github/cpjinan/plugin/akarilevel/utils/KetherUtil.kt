379ea4f71da3cd3484c7f02fd7af946c8178a9ae
CPJiNan <2759278070@qq.com>
2024-02-15 19:31:20 +0800

    [3.0.7] [+]更新KetherUtil

--- src/main/kotlin/com/github/cpjinan/plugin/akarilevel/utils/KetherUtil.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akarilevel/utils/KetherUtil.kt
@@ -1,10 +1,11 @@
 package com.github.cpjinan.plugin.akarilevel.utils
 
-import org.bukkit.entity.Player
-import taboolib.common.platform.function.adaptCommandSender
-import taboolib.module.kether.*
+import taboolib.common.platform.function.adaptPlayer
+import taboolib.common5.util.replace
+import taboolib.module.kether.KetherShell
+import taboolib.module.kether.ScriptOptions
+import taboolib.module.kether.printKetherErrorMessage
 import taboolib.platform.BukkitPlugin
-import java.util.concurrent.CompletableFuture
 
 /**
  * kether util
@@ -12,110 +13,29 @@ import java.util.concurrent.CompletableFuture
  * @since 2022-08-19
  */
 object KetherUtil {
-    /**
-     * run Kether
-     * @param [player] player
-     * @return [Boolean]
-     */
-    fun String.runKether(player: Player? = null): Boolean {
-        var result = false
-        try {
-            val script = if (this.startsWith("def")) {
-                this
-            } else {
-                "def main = { $this }"
-            }
-            script.toKetherScript().runActions {
-                this.sender = player?.let { adaptCommandSender(it) }
-            }.thenAccept {
-                if (it != false) result = true
-            }
-        } catch (e: Exception) {
-            e.printKetherErrorMessage()
-        }
-        return result
-    }
-
-    /**
-     * eval kether
-     * @param [player] player
-     * @return [Any?]
-     */
-    fun String.evalKether(player: Player? = null): Any? {
-        var result: Any? = null
-        try {
-            val script = if (this.startsWith("def")) {
-                this
-            } else {
-                "def main = { $this }"
-            }
-            script.toKetherScript().runActions {
-                this.sender = player?.let { adaptCommandSender(it) }
-            }.thenAccept {
-                result = it
-            }
-        } catch (e: Exception) {
-            e.printKetherErrorMessage()
-        }
-        return result
-    }
-
-    /**
-     * to kether script
-     * @param [namespace] namespace
-     * @return [Script]
-     */
-    fun String.toKetherScript(namespace: List<String> = listOf(BukkitPlugin.getInstance().name)): Script {
-        return if (namespace.contains(BukkitPlugin.getInstance().name)) {
-            this.parseKetherScript(namespace)
-        } else {
-            this.parseKetherScript(namespace.plus(BukkitPlugin.getInstance().name))
-        }
-    }
-
-    /**
-     * run actions
-     * @param [func] function
-     * @return [CompletableFuture<Any?>]
-     */
-    fun Script.runActions(func: ScriptContext.() -> Unit): CompletableFuture<Any?> {
-        return try {
-            ScriptContext.create(this).apply(func).runActions()
-        } catch (e: Exception) {
-            e.printKetherErrorMessage()
-            CompletableFuture.completedFuture(null)
-        }
-    }
-
-    /**
-     * eval actions
-     * @param [script] script
-     * @param [sender] sender
-     * @param [namespace] namespace
-     * @param [args] args
-     * @param [throws] throws
-     * @return [CompletableFuture<Any?>]
-     */
-    @Deprecated("")
-    fun eval(
-        script: String,
-        sender: Any? = null,
+    fun String.evalKether(
+        sender: Any,
         namespace: List<String> = listOf(BukkitPlugin.getInstance().name),
-        args: Map<String, Any?>? = null,
-        throws: Boolean = false
-    ): CompletableFuture<Any?> {
-        val func = {
-            @Suppress("DEPRECATION")
-            KetherShell.eval(script, sender = sender?.let { adaptCommandSender(it) }, namespace = namespace, context = {
-                args?.forEach { (k, v) -> set(k, v) }
-            })
-        }
-        return if (throws) func()
-        else try {
-            func()
+        args: Map<String, Any>? = null,
+    ): Any? {
+        return eval(listOf(this), sender, namespace, args)
+    }
+
+    private fun eval(
+        script: List<String>,
+        sender: Any,
+        namespace: List<String> = listOf(BukkitPlugin.getInstance().name),
+        args: Map<String, Any>? = null,
+    ): Any? {
+        args?.forEach { (k, v) -> script.replace(Pair(k, v)) }
+        try {
+            return KetherShell.eval(
+                script,
+                ScriptOptions.builder().namespace(namespace).sender(adaptPlayer(sender)).build()
+            ).thenApply { it }
         } catch (e: Exception) {
             e.printKetherErrorMessage()
-            CompletableFuture.completedFuture(false)
+            return null
         }
     }
 }
