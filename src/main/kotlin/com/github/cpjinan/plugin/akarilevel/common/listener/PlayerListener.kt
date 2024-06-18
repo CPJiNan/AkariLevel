6275915faefbad8c4d9f9c0f6135059ec4f9989b
季楠 <2759278070@qq.com>
2024-06-18 22:28:39 +0800

    [4.0.0] [+]准备插件重写工作

---
+++ src/main/kotlin/com/github/cpjinan/plugin/akarilevel/common/listener/PlayerListener.kt
@@ -0,0 +1,30 @@
+package com.github.cpjinan.plugin.akarilevel.common.listener
+
+import com.github.cpjinan.plugin.akarilevel.api.DataAPI
+import com.github.cpjinan.plugin.akarilevel.api.LevelAPI
+import com.github.cpjinan.plugin.akarilevel.api.PlayerAPI
+import org.bukkit.event.player.PlayerExpChangeEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import taboolib.common.platform.event.SubscribeEvent
+
+object PlayerListener {
+    @SubscribeEvent
+    fun onPlayerExpChange(event: PlayerExpChangeEvent) {
+        LevelAPI.getLevelGroupNames().forEach {
+            PlayerAPI.addPlayerExp(event.player, it, event.amount, "VANILLA_EXP_CHANGE")
+            PlayerAPI.refreshPlayerLevel(event.player, it)
+        }
+        event.amount = 0
+    }
+
+    @SubscribeEvent
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        PlayerAPI.refreshPlayerLevel(event.player, "LISTENER_PLAYER_JOIN")
+    }
+
+    @SubscribeEvent
+    fun onPlayerQuit(event: PlayerQuitEvent) {
+        DataAPI.saveData()
+    }
+}
