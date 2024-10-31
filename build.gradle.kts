b23d59e39ca6bf6e11a28986dbca97aa8a447bc5
季楠 <2759278070@qq.com>
2024-10-31 17:11:07 +0800

    [4.0.6] [+]更新 Taboolib 版本至 6.2.0-beta26 [+]修改部分 API [+]新增部分变量 [+]新增对 AttributePlus 2.x 版本的支持 [-]删除暂未使用的 UI 模块

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.izzel.taboolib.gradle.*
 plugins {
     `java-library`
     `maven-publish`
-    id("io.izzel.taboolib") version "2.0.19"
+    id("io.izzel.taboolib") version "2.0.20"
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.22"
 }
@@ -31,7 +31,7 @@ taboolib {
             name("DungeonPlus").optional(true)
         }
     }
-    version { taboolib = "6.2.0-beta23" }
+    version { taboolib = "6.2.0-beta26" }
     relocate("kotlinx.serialization", "kotlinx.serialization162")
 }
 
