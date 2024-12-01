2890233e5eb880cf714aa7ab23d0722eecb69d0e
季楠 <2759278070@qq.com>
2024-12-01 16:27:28 +0800

    [4.0.7] [+]新增脚本相关内容

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.izzel.taboolib.gradle.*
 plugins {
     `java-library`
     `maven-publish`
-    id("io.izzel.taboolib") version "2.0.21"
+    id("io.izzel.taboolib") version "2.0.22"
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.22"
 }
