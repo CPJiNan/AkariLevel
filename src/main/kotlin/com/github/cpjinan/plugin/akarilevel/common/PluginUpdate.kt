43d0adfc905a71cdb63e867cff336ed3fde38e82
季楠 <2759278070@qq.com>
2024-12-19 12:12:02 +0800

    [4.0.10] [+]新增若干内容

--- src/main/kotlin/com/github/cpjinan/plugin/akarilevel/common/PluginUpdate.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akarilevel/common/PluginUpdate.kt
@@ -1,7 +1,6 @@
 package com.github.cpjinan.plugin.akarilevel.common
 
 import com.github.cpjinan.plugin.akarilevel.utils.core.LoggerUtil.message
-import com.github.cpjinan.plugin.akarilevel.utils.core.VersionUtil.toVersion
 import org.bukkit.entity.Player
 import taboolib.common.platform.function.console
 import taboolib.common5.util.replace
@@ -33,9 +32,9 @@ object PluginUpdate {
             val urlConnection =
                 URL("https://cpjinan.github.io/Pages/AkariLevel/version.html").openConnection() as HttpURLConnection
             try {
-                val latestVersion = urlConnection.inputStream.bufferedReader().readText().toVersion()!!
-                val currentVersion = BukkitPlugin.getInstance().description.version.toVersion()!!
-                if (latestVersion > currentVersion) {
+                val latestVersion = urlConnection.inputStream.bufferedReader().readText()
+                val currentVersion = BukkitPlugin.getInstance().description.version
+                if (latestVersion != currentVersion) {
                     console().asLangTextList("Plugin-Update")
                         .replace(Pair("%latestVersion%", latestVersion), Pair("%currentVersion%", currentVersion))
                         .forEach {
@@ -55,11 +54,11 @@ object PluginUpdate {
             val urlConnection =
                 URL("https://cpjinan.github.io/Pages/AkariLevel/version.html").openConnection() as HttpURLConnection
             try {
-                val latestVersion = urlConnection.inputStream.bufferedReader().readText().toVersion()!!
-                val currentVersion = BukkitPlugin.getInstance().description.version.toVersion()!!
-                if (latestVersion > currentVersion) {
+                val latestVersion = urlConnection.inputStream.bufferedReader().readText()
+                val currentVersion = BukkitPlugin.getInstance().description.version
+                if (latestVersion != currentVersion) {
                     player.asLangTextList("Plugin-Update")
-                        .replace(Pair("%latestVersion%", latestVersion), Pair("%currentVersion%", currentVersion))
+                        .replace("%latestVersion%" to latestVersion, "%currentVersion%" to currentVersion)
                         .forEach {
                             player.sendMessage(it.colored())
                         }
@@ -77,7 +76,7 @@ object PluginUpdate {
         val currentVersion = PluginConfig.getConfigVersion()
         if (currentVersion < latestVersion) {
             console().asLangTextList("Config-Update")
-                .replace(Pair("%latestVersion%", latestVersion), Pair("%currentVersion%", currentVersion))
+                .replace("%latestVersion%" to latestVersion, "%currentVersion%" to currentVersion)
                 .forEach {
                     message(it.colored())
                 }
