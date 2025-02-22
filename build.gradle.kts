4e82169a1cb4f8f296f392db7d9d2354a352684f
季楠 <2759278070@qq.com>
2025-02-22 11:41:06 +0800

    [4.0.11] [+]新增若干内容

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     `java-library`
     `maven-publish`
     id("io.izzel.taboolib") version "2.0.22"
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm") version "1.9.22"
 }
 
 taboolib {
