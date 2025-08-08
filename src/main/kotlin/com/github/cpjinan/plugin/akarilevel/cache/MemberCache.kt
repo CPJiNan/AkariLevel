b166deb991713a13f5e128b7b067beda0eca975e
季楠 <2759278070@qq.com>
2025-08-08 22:11:01 +0800

    [5.0.0] [+]新增若干内容

--- src/main/kotlin/com/github/cpjinan/plugin/akarilevel/cache/MemberCache.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akarilevel/cache/MemberCache.kt
@@ -4,6 +4,7 @@ import com.github.benmanes.caffeine.cache.Caffeine
 import com.github.cpjinan.plugin.akarilevel.entity.MemberData
 import com.github.cpjinan.plugin.akarilevel.manager.DatabaseManager
 import com.google.gson.Gson
+import taboolib.common.platform.function.submit
 import java.util.concurrent.TimeUnit
 
 /**
@@ -20,6 +21,13 @@ private val gson: Gson = Gson()
 val memberCache = Caffeine.newBuilder()
     .maximumSize(100)
     .refreshAfterWrite(5, TimeUnit.MINUTES)
+    .removalListener<String, MemberData> { key, value, _ ->
+        submit(async = true) {
+            if (key != null && value != null) {
+                DatabaseManager.getDatabase()[key] = gson.toJson(value)
+            }
+        }
+    }
     .build<String, MemberData> {
         DatabaseManager.getDatabase()[it].takeUnless { it.isNullOrBlank() }
             ?.let { gson.fromJson(it, MemberData::class.java) } ?: MemberData()
