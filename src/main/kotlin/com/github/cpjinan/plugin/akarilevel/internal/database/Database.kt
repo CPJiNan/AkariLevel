ee407bc0a225bd5fabee09552f5c91e38184f439
季楠 <2759278070@qq.com>
2025-08-13 23:58:19 +0800

    [4.0.12] [+]新增若干内容

--- src/main/kotlin/com/github/cpjinan/plugin/akarilevel/internal/database/Database.kt
+++
@@ -1,7 +0,0 @@
-package com.github.cpjinan.plugin.akarilevel.internal.database
-
-interface Database {
-    fun getValue(table: String, index: String, key: String): String
-    fun setValue(table: String, index: String, key: String, value: String)
-    fun save()
-}
--- src/main/kotlin/com/github/cpjinan/plugin/akarilevel/internal/database/type/PlayerData.kt
+++
@@ -1,3 +0,0 @@
-package com.github.cpjinan.plugin.akarilevel.internal.database.type
-
-data class PlayerData(var level: Long = 0, var exp: Long = 0)
