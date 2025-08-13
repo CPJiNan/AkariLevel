6180b98b796b261dc32f387b8f8251eea90ee799
季楠 <2759278070@qq.com>
2025-08-13 23:58:19 +0800

    [4.0.12] [+]新增若干内容

--- src/main/kotlin/com/github/cpjinan/plugin/akarilevel/AkariLevel.kt
+++
@@ -1,8 +0,0 @@
-package com.github.cpjinan.plugin.akarilevel
-
-import taboolib.common.platform.Plugin
-import taboolib.platform.BukkitPlugin
-
-object AkariLevel : Plugin() {
-    val plugin by lazy { BukkitPlugin.getInstance() }
-}
