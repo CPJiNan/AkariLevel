e4fc7667cc44b7e5fc8cdf749cc97e0bd897b5af
季楠 <2759278070@qq.com>
2025-08-17 21:50:27 +0800

    feat: 新增变量 ✨

---
+++ src/main/kotlin/com/github/cpjinan/plugin/akarilevel/hook/PlaceholderAPIHook.kt
@@ -0,0 +1,121 @@
+package com.github.cpjinan.plugin.akarilevel.hook
+
+import com.github.cpjinan.plugin.akarilevel.level.ConfigLevelGroup
+import com.github.cpjinan.plugin.akarilevel.level.LevelGroup
+import org.bukkit.OfflinePlayer
+import taboolib.common.platform.function.console
+import taboolib.common5.util.createBar
+import taboolib.module.chat.colored
+import taboolib.module.lang.asLangText
+import taboolib.platform.compat.PlaceholderExpansion
+
+/**
+ * AkariLevel
+ * com.github.cpjinan.plugin.akarilevel.hook
+ *
+ * PlaceholderAPI 挂钩。
+ *
+ * @author 季楠
+ * @since 2025/8/17 20:07
+ */
+object PlaceholderAPIHook : PlaceholderExpansion {
+    /**
+     * 变量前缀。
+     */
+    override val identifier = "akarilevel"
+
+    /**
+     * 离线玩家请求变量事件。
+     */
+    override fun onPlaceholderRequest(player: OfflinePlayer?, args: String): String {
+        val argsList = args.split("_")
+        val notAvailable = console().asLangText("PlaceholderNotAvailable")
+        if (player == null) return notAvailable
+        val playerName = player.name
+        val levelGroup = LevelGroup.getLevelGroups()[argsList[0]]
+        if (levelGroup == null) return notAvailable
+        val configLevelGroup = ConfigLevelGroup.getConfigLevelGroups()[levelGroup.name]
+        val currentLevel = levelGroup.getMemberLevel(playerName)
+        val lastLevel = currentLevel - 1
+        val nextLevel = currentLevel + 1
+        val minLevel = configLevelGroup?.getMinLevel()
+        val maxLevel = configLevelGroup?.getMaxLevel()
+        val currentExp = levelGroup.getMemberExp(playerName)
+        val nextLevelExp = levelGroup.getLevelExp(currentLevel, currentLevel + 1)
+
+        return when (argsList[1].lowercase()) {
+            // 等级组名称。
+            "name" -> levelGroup.name
+            "display" -> levelGroup.display.colored()
+
+            // 等级。
+            "level" -> currentLevel
+            "lastlevel" -> lastLevel
+            "nextlevel" -> nextLevel
+            "minlevel" -> minLevel ?: notAvailable
+            "maxlevel" -> maxLevel ?: notAvailable
+
+            // 经验。
+            "exp" -> currentExp
+
+            // 等级名称。
+            "levelname" -> levelGroup.getLevelName(playerName, currentLevel).colored()
+            "lastlevelname" -> levelGroup.getLevelName(playerName, currentLevel).colored()
+            "nextlevelname" -> levelGroup.getLevelName(playerName, currentLevel).colored()
+
+            // 升级所需经验。
+            "levelexp" -> levelGroup.getLevelExp(currentLevel - 1, currentLevel)
+            "lastlevelexp" -> levelGroup.getLevelExp(currentLevel - 2, currentLevel - 1)
+            "nextlevelexp" -> nextLevelExp
+
+            "levelexpfrom" -> {
+                val oldLevel = argsList[2].toLongOrNull() ?: return notAvailable
+                if (oldLevel > currentLevel) return notAvailable
+                levelGroup.getLevelExp(oldLevel, currentLevel)
+            }
+
+            "levelexpto" -> {
+                val newLevel = argsList[2].toLongOrNull() ?: return notAvailable
+                if (newLevel < currentLevel) return notAvailable
+                levelGroup.getLevelExp(currentLevel, newLevel)
+            }
+
+            "levelexpfromto" -> {
+                val oldLevel = argsList[2].toLongOrNull() ?: return notAvailable
+                val newLevel = argsList[2].toLongOrNull() ?: return notAvailable
+                if (oldLevel > newLevel) return notAvailable
+                levelGroup.getLevelExp(oldLevel, newLevel)
+            }
+
+            // 升级进度百分比。
+            "levelprogresspercent" -> {
+                if (maxLevel == null) return notAvailable
+                (currentLevel.toDouble() / maxLevel * 100).coerceIn(0.0, 100.0).toLong()
+            }
+
+            "expprogresspercent" -> {
+                (currentExp.toDouble() / nextLevelExp * 100).coerceIn(0.0, 100.0).toLong()
+            }
+
+            // 升级进度条。
+            "levelprogressbar" -> {
+                if (maxLevel == null) return notAvailable
+                createBar(
+                    console().asLangText("PlaceholderLevelBarEmpty").colored(),
+                    console().asLangText("PlaceholderLevelBarEmpty").colored(),
+                    console().asLangText("PlaceholderLevelBarLength").toInt(),
+                    (currentLevel.toDouble() / maxLevel).coerceIn(0.0, 1.0)
+                )
+            }
+
+            "expprogressbar" -> createBar(
+                console().asLangText("PlaceholderExpBarEmpty").colored(),
+                console().asLangText("PlaceholderExpBarEmpty").colored(),
+                console().asLangText("PlaceholderExpBarLength").toInt(),
+                (currentExp.toDouble() / nextLevelExp).coerceIn(0.0, 1.0)
+            )
+
+            else -> notAvailable
+        }.toString()
+    }
+}
