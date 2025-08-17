69b2c359b9c5d9acd47ff9d5c61abff2bd3b11ef
季楠 <2759278070@qq.com>
2025-08-17 14:32:19 +0800

    feat: 新增脚本 ✨

--- src/main/kotlin/com/github/cpjinan/plugin/akarilevel/command/MainCommand.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akarilevel/command/MainCommand.kt
@@ -2,7 +2,6 @@ package com.github.cpjinan.plugin.akarilevel.command
 
 import com.github.cpjinan.plugin.akarilevel.command.subcommand.LevelGroupCommand
 import com.github.cpjinan.plugin.akarilevel.command.subcommand.MemberCommand
-import com.github.cpjinan.plugin.akarilevel.config.PlayerConfig
 import com.github.cpjinan.plugin.akarilevel.config.SettingsConfig
 import com.github.cpjinan.plugin.akarilevel.event.PluginReloadEvent
 import com.github.cpjinan.plugin.akarilevel.level.ConfigLevelGroup
@@ -55,7 +54,6 @@ object MainCommand {
 
             // 重载配置文件。
             SettingsConfig.settings.reload()
-            PlayerConfig.player.reload()
 
             // 重载配置等级组。
             ConfigLevelGroup.reloadConfigLevelGroups()
