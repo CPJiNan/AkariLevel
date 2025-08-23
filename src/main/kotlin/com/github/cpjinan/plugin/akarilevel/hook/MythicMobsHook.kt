657004e95a71d0e57c6a8a7f8b00a437b31764c4
季楠 <2759278070@qq.com>
2025-08-23 18:47:15 +0800

    feat: 新增 MythicMobs 经验掉落 ✨

--- src/main/kotlin/com/github/cpjinan/plugin/akarilevel/hook/MythicMobsHook.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akarilevel/hook/MythicMobsHook.kt
@@ -1,9 +1,10 @@
 package com.github.cpjinan.plugin.akarilevel.hook
 
+import com.github.cpjinan.plugin.akarilevel.level.LevelGroup
 import io.lumine.mythic.bukkit.events.MythicMobDeathEvent
 import org.bukkit.entity.Player
-import taboolib.common.platform.Ghost
 import taboolib.common.platform.event.SubscribeEvent
+import kotlin.random.Random
 import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMobDeathEvent as LegacyMythicMobDeathEvent
 
 /**
@@ -16,23 +17,47 @@ import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMobDeathEvent as Lega
  * @since 2025/8/23 11:13
  */
 object MythicMobsHook {
-    @Ghost
     @SubscribeEvent
     fun onLegacyMythicMobDeath(event: LegacyMythicMobDeathEvent) {
         if (event.killer is Player) {
             val player = event.killer as Player
             val drops = event.mobType.config.getStringList("Exp-Drop") +
                     event.mobType.config.getStringList("Exp-Drops")
+            drops.forEach { drop ->
+                val args = drop.split(" ")
+                val levelGroup = LevelGroup.getLevelGroups()[args[0]] ?: return@forEach
+                val amount = args[1].run {
+                    if (contains("-")) {
+                        val (origin, bound) = split("-").map { it.toLong() }
+                        (origin..bound).random()
+                    } else toLong()
+                }
+                if (Random.nextDouble() < args[2].toDouble()) {
+                    levelGroup.addMemberExp(player.name, amount, "MYTHICMOBS_DROP_EXP")
+                }
+            }
         }
     }
 
-    @Ghost
     @SubscribeEvent
     fun onMythicMobDeath(event: MythicMobDeathEvent) {
         if (event.killer is Player) {
             val player = event.killer as Player
             val drops = event.mobType.config.getStringList("Exp-Drop") +
                     event.mobType.config.getStringList("Exp-Drops")
+            drops.forEach { drop ->
+                val args = drop.split(" ")
+                val levelGroup = LevelGroup.getLevelGroups()[args[0]] ?: return@forEach
+                val amount = args[1].run {
+                    if (contains("-")) {
+                        val (origin, bound) = split("-").map { it.toLong() }
+                        (origin..bound).random()
+                    } else toLong()
+                }
+                if (Random.nextDouble() < args[2].toDouble()) {
+                    levelGroup.addMemberExp(player.name, amount, "MYTHICMOBS_DROP_EXP")
+                }
+            }
         }
     }
 }
