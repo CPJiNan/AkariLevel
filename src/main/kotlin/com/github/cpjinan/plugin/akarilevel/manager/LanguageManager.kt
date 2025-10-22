b7e6a7f4f3218e47389adfbd74db84af6312c559
季楠 <2759278070@qq.com>
2025-10-22 11:51:24 +0800

    feat: 修改 LanguageManager.kt

--- src/main/kotlin/com/github/cpjinan/plugin/akarilevel/manager/LanguageManager.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akarilevel/manager/LanguageManager.kt
@@ -2,12 +2,8 @@ package com.github.cpjinan.plugin.akarilevel.manager
 
 import com.github.cpjinan.plugin.akarilevel.config.SettingsConfig
 import taboolib.common.platform.event.SubscribeEvent
-import taboolib.module.lang.Language
 import taboolib.module.lang.event.PlayerSelectLocaleEvent
 import taboolib.module.lang.event.SystemSelectLocaleEvent
-import taboolib.module.lang.registerLanguage
-import taboolib.platform.util.bukkitPlugin
-import java.io.File
 
 /**
  * AkariLevel
@@ -24,20 +20,7 @@ object LanguageManager {
      */
     @SubscribeEvent
     fun onPlayerSelectLocale(event: PlayerSelectLocaleEvent) {
-        val lang = SettingsConfig.language
-        val isLangExist = lang in Language.languageCode
-        val langFile = File(bukkitPlugin.dataFolder, "lang${File.separator}$lang.yml")
-
-        // 从配置文件获取语言选项。
-        if (langFile.exists()) {
-            if (!isLangExist) {
-                registerLanguage(lang)
-            }
-            event.locale = lang
-        } else {
-            // 如果指定语言不存在，默认使用 zh_CN 。
-            event.locale = "zh_CN"
-        }
+        event.locale = SettingsConfig.language
     }
 
     /**
@@ -45,19 +28,6 @@ object LanguageManager {
      */
     @SubscribeEvent
     fun onSystemSelectLocale(event: SystemSelectLocaleEvent) {
-        val lang = SettingsConfig.language
-        val isLangExist = lang in Language.languageCode
-        val langFile = File(bukkitPlugin.dataFolder, "lang${File.separator}$lang.yml")
-
-        // 从配置文件获取语言选项。
-        if (langFile.exists()) {
-            if (!isLangExist) {
-                registerLanguage(lang)
-            }
-            event.locale = lang
-        } else {
-            // 如果指定语言不存在，默认使用 zh_CN 。
-            event.locale = "zh_CN"
-        }
+        event.locale = SettingsConfig.language
     }
 }
