f48a245f73a7ee85840553fb38a832da5856796d
季楠 <2759278070@qq.com>
2025-12-04 23:13:29 +0800

    feat: 修改 BoosterHandler.kt

--- src/main/kotlin/top/cpjinan/akarilevel/booster/BoosterData.kt
+++ src/main/kotlin/top/cpjinan/akarilevel/booster/BoosterData.kt
@@ -11,7 +11,6 @@ package top.cpjinan.akarilevel.booster
  */
 data class BoosterData(
     var name: String = "",
-    var active: Boolean = true,
     var type: String = "",
     var multiplier: Double = 1.0,
     var start: Long = -1,
