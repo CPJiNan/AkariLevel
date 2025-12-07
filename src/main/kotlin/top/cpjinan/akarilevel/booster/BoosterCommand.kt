cb47913176fb581c4761d26d948301e71243c6ef
季楠 <2759278070@qq.com>
2025-12-07 14:59:20 +0800

    feat: 修改 BoosterHandler.kt

--- src/main/kotlin/top/cpjinan/akarilevel/booster/BoosterCommand.kt
+++ src/main/kotlin/top/cpjinan/akarilevel/booster/BoosterCommand.kt
@@ -1,5 +1,13 @@
 package top.cpjinan.akarilevel.booster
 
+import taboolib.common.platform.ProxyCommandSender
+import taboolib.common.platform.command.CommandBody
+import taboolib.common.platform.command.CommandHeader
+import taboolib.common.platform.command.PermissionDefault
+import taboolib.common.platform.command.mainCommand
+import taboolib.module.lang.sendLang
+import taboolib.platform.util.bukkitPlugin
+
 /**
  * AkariLevel
  * top.cpjinan.akarilevel.booster
@@ -9,8 +17,24 @@ package top.cpjinan.akarilevel.booster
  * @author 季楠
  * @since 2025/12/7 14:32
  */
+@CommandHeader(
+    name = "akarilevelbooster",
+    aliases = ["alb"],
+    permission = "AkariLevel.command.booster.use",
+    permissionDefault = PermissionDefault.OP
+)
 object BoosterCommand {
-    fun parseCommandArgs(args: String): Map<String, String> {
+    @CommandBody(
+        permission = "AkariLevel.command.booster.use",
+        permissionDefault = PermissionDefault.OP
+    )
+    val main = mainCommand {
+        execute<ProxyCommandSender> { sender, _, _ ->
+            sender.sendLang("CommandHelpBooster", bukkitPlugin.description.version)
+        }
+    }
+
+    private fun parseCommandArgs(args: String): Map<String, String> {
         return args.split(' ')
             .filter { it.startsWith("-") }
             .associate {
