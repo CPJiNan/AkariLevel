180301b122199090ce5e6fcab2d61f8d954898eb
季楠 <2759278070@qq.com>
2025-12-07 15:23:27 +0800

    feat: 修改 BoosterHandler.kt

--- src/main/kotlin/top/cpjinan/akarilevel/booster/BoosterEvent.kt
+++ src/main/kotlin/top/cpjinan/akarilevel/booster/BoosterEvent.kt
@@ -1,7 +1,6 @@
 package top.cpjinan.akarilevel.booster
 
 import taboolib.platform.type.BukkitProxyEvent
-import top.cpjinan.akarilevel.event.MemberExpChangeEvent
 import java.util.*
 
 /**
@@ -14,7 +13,10 @@ import java.util.*
  * @since 2025/12/6 21:14
  */
 class BoosterEvent(
-    val memberExpChangeEvent: MemberExpChangeEvent,
+    val member: String,
+    val levelGroup: String,
+    val expAmount: Long,
+    val source: String,
     val boosters: Map<UUID, BoosterData>,
     var multiplier: Double
 ) : BukkitProxyEvent()
